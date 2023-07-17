84d90d4fead17a124120bdc989e7239f73b26493
Mirrie572 <maggie05072000@gmail.com>
2023-07-16 20:32:29 -0700

    using for loop for the first time

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - a program prints base 10 numbers
+ * Return: 0 impy success
+ */
+
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit <= 9999; digit++)
+{
+printf("%d\n", digit); }
+
+return (0);
+}
