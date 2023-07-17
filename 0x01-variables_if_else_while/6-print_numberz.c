76fbbd447f28412c47703acf468073fcf9e1f139
Mirrie572 <maggie05072000@gmail.com>
2023-07-17 00:55:04 -0700

    single digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - a program prints base 10 numbers
+ * Return: 0 impy success
+ */
+
+int main(void)
+{
+int digit;
+
+for (digit = 0; digit < 10; digit++)
+{
+putchar('0' + digit)
+putchar('\n');
+}
+
+return (0);
+}
