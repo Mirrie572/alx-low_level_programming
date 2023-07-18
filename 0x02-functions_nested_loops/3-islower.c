1e9eabdc98ef7c70912a80b66fa2e4d9123db140
Mirrie572 <maggie05072000@gmail.com>
2023-07-18 07:41:37 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - a program description
+ * @c: is the character to be checked on
+ * Return: 0 or 1 at the end
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
