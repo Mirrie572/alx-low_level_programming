8252e0c6e6bd30dec58466e213f30a894d7f8d83
Mirrie572 <maggie05072000@gmail.com>
2023-08-16 17:40:27 -0700

    WHATS YOUR NAME

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stdio.h> 
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -15,5 +15,6 @@ void print_name(char *name, void (*f)(char *))
 {
 
 	if (name != NULL && f != NULL)
-	f(name);
+		f(name);
+
 }
