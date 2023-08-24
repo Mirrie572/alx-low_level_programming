f4540bad69782dbb22ac71985ff95e92d1f163cf
Mirriam Maggiefwamba Mwawa <maggie05072000@gmail.com>
2023-08-24 16:05:59 -0700

    Task 0, prints list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,12 +4,26 @@
  * print_list - prints all elements of a list
  * @h: pointer for the list
  *
- *Return: number of nodes
+ * Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
+	size_t count = 0; /* Number of nodes */
 
+	while (h != NULL)
+	{
+		if (h->str == NULL)
 
+		{printf("[0] (nil)\n"); }
+
+		else
+		{ printf("[%u] %s\n", h->len, h->str); }
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 
 }
