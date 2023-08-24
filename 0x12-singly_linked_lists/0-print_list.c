3353ea5fa634ffcdd04c1f15c171509ed2605d38
Mirriam Maggiefwamba Mwawa <maggie05072000@gmail.com>
2023-08-24 08:30:01 -0700

    new pat trial

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: pointer for the list
+ *
+ *Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+
+
+
+}
